78c56183d4d29b299bab62e89221be8da044b250
Warrior07a <akshatmittalofficial@gmail.com>
2026-05-06 19:28:23 +0530

    feat : sidebars added

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -27,6 +27,11 @@ export default function RootLayout({
         geist.variable
       )}
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Outfit:wght@100..900&family=Orbitron:wght@400..900&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
